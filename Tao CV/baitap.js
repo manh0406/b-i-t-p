887383948a152674a58699edf1ec18561a6c858f
manh <manhd9035@gmail.com>
2024-09-19 15:54:35 +0700

    khoi tao

---
+++ Tao CV/baitap.js
@@ -0,0 +1,6 @@
+/function: hàm
+// addEventListener: lắng nghe sự kiện click.
+// Khi sự kiện click xảy ra thì nó sẽ thực hiện function và hiển thị
+document.getElementById("btn").addEventListener("click", function () {
+    alert("Hello")
+})
