b060c75c10055a741ef9bf2b1c06e4748d1e1f8f
manh <manhd9035@gmail.com>
2024-09-19 12:59:16 +0700

    khoi tao

---
+++ ss6/like.js
@@ -0,0 +1,17 @@
+// function: hàm
+// addEventListener: lắng nghe sự kiện click.
+// Khi sự kiện click xảy ra thì nó sẽ thực hiện function và hiển thị
+let btn : HTMLElement = document.getElementById("btn")
+btn.addEventListener("click", function(){
+    btn.style.backgroundColor = "blue";
+    btn.style.color="white";
+    btn.innerText="Unlike";
+
+})
+
+let a = prompt("a");
+console.log(a)
+let b = prompt("b");
+console.log(b)
+alert(a + b)
+
