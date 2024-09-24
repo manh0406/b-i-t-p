73f1abece470101a3bc58da9643625ba043dfc13
manh <manhd9035@gmail.com>
2024-09-24 19:15:13 +0700

    khoi tao

--- Tao CV/baitap.js
+++ Tao CV/baitap.js
@@ -1,6 +1,21 @@
-/function: hàm
+//function: hàm
 // addEventListener: lắng nghe sự kiện click.
 // Khi sự kiện click xảy ra thì nó sẽ thực hiện function và hiển thị
-document.getElementById("btn").addEventListener("click", function () {
-    alert("Hello")
-})
+//document.getElementById("btn").addEventListener("click", function () {
+ //   alert("Hello")
+//})
+let userName = prompt("who is there?", '');
+if (userName == 'Admin'){
+    let pass = prompt('Password?', '');
+    if(pass == 'TheMaster') {
+        alert('Welcome');
+    }else if(pass == null){
+        alert('Canceled');
+    } else{
+        alert('Wrong password');}
+}else if (userName == null){
+    alert('Canceled');
+
+}else{
+    alert("I don't know you");
+}
--- ss6/like.js
+++ ss6/like.js
@@ -1,7 +1,7 @@
 // function: hàm
 // addEventListener: lắng nghe sự kiện click.
 // Khi sự kiện click xảy ra thì nó sẽ thực hiện function và hiển thị
-let btn : HTMLElement = document.getElementById("btn")
+/*let btn : HTMLElement = document.getElementById("btn")
 btn.addEventListener("click", function(){
     btn.style.backgroundColor = "blue";
     btn.style.color="white";
@@ -14,4 +14,5 @@ console.log(a)
 let b = prompt("b");
 console.log(b)
 alert(a + b)
+*/
 
