867f8e55a017531f3e8da525d1b5a7e943408ff9
manh <manhd9035@gmail.com>
2024-10-15 16:13:50 +0700

    khoi tao

--- OOP/Rectangle.js
+++ OOP/Rectangle.js
@@ -1,9 +1,31 @@
 class Rectangle {
-    width;
-    height;
-    constructor(width, height) {
+    constructor(length, width) {
+        this.length = length;
         this.width = width;
-        this.height = height;
+    }
+
+    getArea() {
+        return this.length * this.width;
+    }
+
+    getPerimeter() {
+        return 2 * (this.length + this.width);
+
+    }
+
+    draw(canvas) {
+        const ctx = canvas.getContext('2d');
+        ctx.fillRect(0, 0, this.length, this.width);
     }
 
 }
+const rect = new Rectangle(100, 50);
+const canvas = document.getElementById('myCanvas');
+rect.draw(canvas);
+
+rect.length = 150;
+rect.width = 75;
+rect.draw(canvas);
+
+console.log("Diện tích:", rect.getArea());
+console.log("Chu vi:", rect.getPerimeter());
--- OOP/my.js
+++ OOP/my.js
@@ -7,9 +7,4 @@
 * bawnfd cách mô tả lại đối tườợng thực  tế vào đói tuognwj lập trinhd trong ÔOP
 * TẤT CAE MỌI TỨ ĐỀU*
 Luu ý: tạo 1 class thì cần tạo 1 file có */
-let rec1 = new Rectangle(10, 5);
-let rec2 = new Rectangle(30, 20);
 
-document.write("Chu vi hình chữ nhật 1: " + rec1.getPerimeter());
-document.write(<br/>);
-document.write("Chu vi hình chữ nhật 2: " + rec2.getPerimeter());
