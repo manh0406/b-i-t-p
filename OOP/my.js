6f66d35d3728cd0f3c42500130856effbad635c9
manh <manhd9035@gmail.com>
2024-10-15 16:18:50 +0700

    khoi tao

--- OOP/my.js
+++ OOP/my.js
@@ -7,4 +7,22 @@
 * bawnfd cách mô tả lại đối tườợng thực  tế vào đói tuognwj lập trinhd trong ÔOP
 * TẤT CAE MỌI TỨ ĐỀU*
 Luu ý: tạo 1 class thì cần tạo 1 file có */
+class Temperature {
+    constructor(celsius) {
+        if (celsius < -273) {
+            throw new Error("Nhiệt độ không hợp lệ. Nhiệt độ thấp nhất là -273 độ C.");
+        }
+        this.celsius = celsius;
+    }
 
+    getFahrenheit() {
+        return (this.celsius * 9/5) + 32;
+    }
+
+    getKelvin() {
+        return this.celsius + 273.15;
+    }
+}
+const temperature = new Temperature(25);
+console.log("Nhiệt độ Fahrenheit:", temperature.getFahrenheit());
+console.log("Nhiệt độ Kelvin:", temperature.getKelvin());
